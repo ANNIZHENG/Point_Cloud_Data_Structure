9267a5d35ad93c09992cbedf182fc338e04c34ac
Anni Zheng <annizheng3@gmail.com>
2024-02-18 13:54:16 -0500

    Update and add new octree visualizations

--- tests/convert_las_to_csv.py
+++ tests/convert_las_to_csv.py
@@ -2,11 +2,13 @@ import numpy as np
 import laspy
 
 # Read the LAS file and convert it into a numpy array
-las_file = laspy.read("NYUVIP-Point-Cloud-Data-Structure/data/Light Pole/streetlight_chunked.las")
+las_file = laspy.read(...)
+
 point_cloud = np.vstack((las_file.x, las_file.y, las_file.z)).transpose()
 
 # Write to file
-with open('lightpole.csv', 'w') as file:
+
+with open(..., 'w') as file:
     for i in point_cloud:
         for j in range(len(i)):
             if (j < len(i)-1):
